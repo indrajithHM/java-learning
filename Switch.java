613bec26615a33a9891c25cfbef059e14beeebc1
Indrajith H M <indrajithlikith7.com>
2023-09-06 15:43:09 +0530

    switch commit

---
+++ Switch.java
@@ -0,0 +1,34 @@
+public class Switch {
+    public static void main(String[] args) {
+        
+        int n=4;
+
+        switch (n) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Tuesday");
+                break;
+            case 5:
+                System.out.println("Tuesday");
+                break;
+            case 6:
+                System.out.println("Tuesday");
+                break;
+            case 7:
+                System.out.println("Tuesday");
+                break;
+            default:
+                System.out.println("enter a valid number");
+
+        }
+    }
+}
