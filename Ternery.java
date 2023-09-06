cc26f7a233127c4da8e133b62779cdb543c27771
Indrajith H M <indrajithlikith7.com>
2023-09-06 15:14:49 +0530

    second commit

---
+++ Ternery.java
@@ -0,0 +1,11 @@
+public class Ternery {
+    public static void main(String[] args) {
+        int n=4;
+        String result=null;
+
+        result= n%2==0?"even number":"odd number" ; //to check the number even or oddgit
+
+        System.out.println(result);
+
+    }
+}
