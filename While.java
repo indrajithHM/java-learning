f01fcc2a67605c0149a251b90e74a1e3324c94da
Indrajith H M <indrajithlikith7.com>
2023-09-08 20:11:18 +0530

    loops

---
+++ While.java
@@ -0,0 +1,22 @@
+public class While{
+    public static void main(String[] args) {
+        
+        int i=1;
+      //  int j;
+
+        while (i<=4) {
+            System.out.println("Hi "+ i);
+            int j=1;
+            while (j<=3) {
+
+                System.out.println("Hello "+ j);
+                j++;
+                
+            }
+            i++;
+            
+        }
+
+        System.out.println("bye "+i);
+    }
+}
